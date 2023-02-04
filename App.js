872d8f3e4b02fb422cf6d75663b78f8bfc1649b5
Ranjana <ranjanaKumari0710@gmail.com>
2023-02-04 15:30:35 +0530

    key added to heading list to remove warning

--- App.js
+++ App.js
@@ -1,15 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading1 = React.createElement("h1", { id: "title" }, "Heading 1");
-const heading2 = React.createElement("h2", { id: "title" }, "Heading 2");
-
- 
+const heading1 = React.createElement(
+  "h1",
+  { id: "title", key: "h1" },
+  "Heading 1"
+);
+const heading2 = React.createElement(
+  "h2",
+  { id: "title", key: "h2" },
+  "Heading 2"
+);
 
 const container = React.createElement("div", { id: "container" }, [
   heading1,
   heading2,
 ]);
-
+console.log(heading1);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(container);
