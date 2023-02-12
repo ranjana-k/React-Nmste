375b31d9b3f5b320b8cd9d5d4aa1758fc17ae338
Ranjana <ranjanaKumari0710@gmail.com>
2023-02-13 01:45:53 +0530

    displayig data with object

--- App.js
+++ App.js
@@ -1,69 +1,95 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading1 = React.createElement(
-  "h1",
-  { className: "title", key: "h1" },
-  "Heading 1"
-);
-const heading2 = React.createElement(
-  "h2",
-  { className: "title", key: "h2" },
-  "Heading 2"
-);
-const heading3 = React.createElement(
-  "h2",
-  { className: "title", key: "h3" },
-  "Heading3"
-);
-const header = React.createElement("div", { className: "header" }, [
-  heading1,
-  heading2,
-  heading3,
-]);
+{
+  /**
+   * Header
+   *   - Logo
+   *   - Navbar
+   *   - Cart
+   * Body
+   *   - SearchBar
+   *   - RestrauntList
+   *       - RestrauntCard
+   *            - Image
+   *            - Name
+   *            - Rating
+   *            - Cusines
+   * Footer
+   *   - Link
+   *   - Copywrite
+   *
+   */
+}
 
-//using JSX: header Element created
+const Logo = () => {
+  return (
+    <div className="logo">
+      <a href="/">
+        <img
+          alt="logo"
+          src="https://cdn.octopix.in/uploads/company-logo/2020/11/19/food-villa-pSJVhwoN8KxgwV9jtuB1MlosJ0ejoKfiBiVO1jJPLM61shyarbxVvjIFy3DVpbUML8eBxcUo7BOWXQcd-350x350.jpg"
+        />
+      </a>
+    </div>
+  );
+};
 
-const header5 = <h1>Header5 from React Element</h1>;
-const headerJSX = (
-  <div className="jsx-header">
-    <h1 className="jsx-title">Heading h1 using JSX</h1>
-    <h2 className="jsx-title">Heading h2 using JSX</h2>
-    <h3 className="jsx-title">Heading h3 using JSX</h3>
-  </div>
-);
-
-//using Function Component: header Element created
 const HeaderComponent = () => {
   return (
-    <div className="functionComponent-header">
-      <div className="logo">
-        <a href="/">
-          <img
-            alt="logo"
-            src="https://www.analyticsinsight.net/wp-content/plugins/gs-logo-slider/assets/img/dummy-data/gs-logo-slider-2.png"
-          />
-        </a>
-      </div>
-
-      <div className="search-container">
-        <form action="">
-          <input type="text" placeholder="Search.." name="search" />
-          <button type="submit">
-            <i class="fa fa-search" aria-hidden="true"></i>
-          </button>
-        </form>
-      </div>
-
-      <div className="user-container">
-        <a>
-          <i class="fa fa-user-circle-o" aria-hidden="true"></i>
-        </a>
+    <div className="header">
+      <Logo />
+      <div className="header-nav">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Cart</li>
+        </ul>
       </div>
     </div>
   );
 };
+
+const restrautList = {
+  image:
+    "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/7bb739d7f6ae8b4ce8ed590765dffd2f",
+  name: "Burger King",
+  cuisines: ["Burger", "American"],
+  rating: "4.2",
+};
+const RestrauntCard = () => {
+  return (
+    <div className="card">
+      <img src={restrautList.image} />
+      <h2>{restrautList.name}</h2>
+      <h3>{restrautList.cuisines.join(", ")}</h3>
+      <h3>{restrautList.rating} stars</h3>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div>
+      <RestrauntCard restraunt={restrautList} />
+    </div>
+  );
+};
+
+const Footer = () => {
+  return <h2>Footer</h2>;
+};
+const AppLayout = () => {
+  return (
+    <>
+      <HeaderComponent />
+      <Body />
+      <Footer />
+    </>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(header);
-// root.render(headerJSX);
-root.render(<HeaderComponent />);
+
+root.render(<AppLayout />);
